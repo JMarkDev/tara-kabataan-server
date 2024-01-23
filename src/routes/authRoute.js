509e989ddcbdfba5e7ab3381c174959f43983d38
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-01-23 14:14:09 +0800

    initial

--- src/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/authController')
-const otpController = require('../controllers/otpController')
-
-router.post('/register', authController.handleRegister)
-router.post('/verify-otp', otpController.verifyOTP)
-
-module.exports = router
