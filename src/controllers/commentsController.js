4971b60ee19e28971bc4b531815ebaf083a06623
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-03-06 00:10:03 +0800

    comment added

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,60 +1,80 @@
-const commentsModel = require('../models/commentsModel')
-const date = require('date-and-time');
-const sequelize = require('../configs/database');
-const fs = require('fs');
+const commentsModel = require("../models/commentsModel");
+const date = require("date-and-time");
+const sequelize = require("../configs/database");
+const fs = require("fs");
 
 const addComments = async (req, res) => {
-    const { event_id, event_name, attendees_name, comment } = req.body;
+  const { event_id, user_id, event_name, attendees_name, comment } = req.body;
 
-    try {
-        const createdAt = new Date()
-        const formattedDate = date.format(createdAt, 'YYYY-MM-DD HH:mm:ss') ; 
+  try {
+    const existingComment = await commentsModel.findAll({
+      where: {
+        event_id: event_id,
+        user_id: user_id,
+      },
+    });
 
-        const imageFileNames = [];
-        await Promise.all(
-           req.files.map(async (file) => {
-             const fileType = file.mimetype.split('/')[1];
-             const newFileName = `${file.filename}.${fileType}`;
-     
-             try {
-               await fs.promises.rename(`./uploads/${file.filename}`, `./uploads/${newFileName}`);
-               console.log('Uploaded Success');
-               imageFileNames.push(newFileName);
-             } catch (err) {
-               console.error('Error renaming/uploading file:', err);
-             }
-           })
-         );
-
-        const postcomment = await commentsModel.create({
-            event_id: event_id,
-            event_name: event_name,
-            attendees_name: attendees_name,
-            comment: comment,
-            image: imageFileNames.join(','),
-            created_at: sequelize.literal(`'${formattedDate}'`),
-        })
-
-        return res.status(200).json(postcomment)
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json({Error: 'Add comments error in server'})
+    if (existingComment.length > 0) {
+      return res.status(400).json({
+        status: "error",
+        message: "You have already written feedback on this event!",
+      });
     }
-}
+
+    const createdAt = new Date();
+    const formattedDate = date.format(createdAt, "YYYY-MM-DD HH:mm:ss");
+
+    const imageFileNames = [];
+    await Promise.all(
+      req.files.map(async (file) => {
+        const fileType = file.mimetype.split("/")[1];
+        const newFileName = `${file.filename}.${fileType}`;
+
+        try {
+          await fs.promises.rename(
+            `./uploads/${file.filename}`,
+            `./uploads/${newFileName}`
+          );
+          console.log("Uploaded Success");
+          imageFileNames.push(newFileName);
+        } catch (err) {
+          console.error("Error renaming/uploading file:", err);
+        }
+      })
+    );
+
+    const postcomment = await commentsModel.create({
+      event_id: event_id,
+      user_id: user_id,
+      event_name: event_name,
+      attendees_name: attendees_name,
+      comment: comment,
+      image: imageFileNames.join(","),
+      created_at: sequelize.literal(`'${formattedDate}'`),
+    });
+
+    return res.status(200).json(postcomment);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ Error: "Add comments error in server" });
+  }
+};
 
 const getCommentsByEventId = async (req, res) => {
-    const { event_id } = req.params;
+  const { event_id } = req.params;
 
-    try {
-        const getComments = await commentsModel.findAll({ where: { event_id: event_id } })
-        return res.status(200).json(getComments)
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json({Error: 'Get comments error in server'})
-    }
-}
+  try {
+    const getComments = await commentsModel.findAll({
+      where: { event_id: event_id },
+    });
+    return res.status(200).json(getComments);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ Error: "Get comments error in server" });
+  }
+};
 
 module.exports = {
-    addComments,
-    getCommentsByEventId
-}
+  addComments,
+  getCommentsByEventId,
+};
--- src/models/commentsModel.js
+++ src/models/commentsModel.js
@@ -1,39 +1,47 @@
 const sequelize = require("../configs/database");
 const { DataTypes } = require("sequelize");
 
-const Comments = sequelize.define("event_reviews", {
+const Comments = sequelize.define(
+  "event_reviews",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
     event_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    }, 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
     event_name: {
-        type: DataTypes.STRING(250),
-        allowNull: false
-    }, 
+      type: DataTypes.STRING(250),
+      allowNull: false,
+    },
     attendees_name: {
-        type: DataTypes.STRING(250),
-        allowNull: false
+      type: DataTypes.STRING(250),
+      allowNull: false,
     },
     comment: {
-        type: DataTypes.STRING(450),
-        allowNull:false
-    }, 
+      type: DataTypes.STRING(450),
+      allowNull: false,
+    },
     image: {
-        type: DataTypes.STRING(450),
-        allowNull: true
+      type: DataTypes.STRING(450),
+      allowNull: true,
     },
     created_at: {
-        type: DataTypes.DATE,
-        allowNull: false
-    }
-}, {
-    timestamps: false
-})
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Comments;
